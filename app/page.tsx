894168d8ecfe80f1bd55ea59590b8cb50a5cd43e
nikhil-3008 <125184672+nikhil-3008@users.noreply.github.com>
2023-10-03 15:23:35 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -93,19 +93,6 @@ export default function Home() {
                 </button>
               </div>
             </div>
-            <div>
-              
-
-             
-              <div className="justify-center flex py-10" >
-                <button
-                  type="button"
-                  className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-xl px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-                >
-                  <Link href="/events">Checkout Events</Link>
-                </button>
-              </div>
-            </div>
 
             <div className="w-full">
               <h1
